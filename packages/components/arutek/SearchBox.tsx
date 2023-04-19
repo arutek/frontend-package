65aacb3419ff66a89bf18b0b62ab68dded70b36c
novando <rizkynovando@gmail.com>
2023-04-20 00:28:00 +0700

    seachBox

---
+++ packages/components/arutek/SearchBox.tsx
@@ -0,0 +1,16 @@
+import {useState} from 'react'
+import notification from '@arutek/package-helpers/src/notification'
+
+function SearchBox() {
+  const [cari, setCari] = useState('')
+  return (
+    <div>
+      <input value={cari} type="text" placeholder="Ketik sesuatu..." className="input"/>
+      <div onClick={() => notification.notifySuccess('ANJAY')}>
+        <button className="btn btn-success">Search</button>
+      </div>
+    </div>
+  )
+}
+
+export default SearchBox
