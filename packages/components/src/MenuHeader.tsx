022ab2235b0ccee747408181dd76e452a78d264f
Rizky Novando <rizkynovando@gmail.com>
2023-06-12 01:15:11 +0700

    run script for partner-arpi-dash

--- packages/components/src/MenuHeader.tsx
+++ packages/components/src/MenuHeader.tsx
@@ -13,12 +13,11 @@ interface propsType {
 }
 
 const MenuHeader = ({routerList}:propsType) => {
-  
   return (
     <header className="bg-white">
       <section className="typ-header-regular-normal max-w-7xl mx-auto p-20">
         <nav className="flex gap-24">
-          {routerList.map((item, key) => {return ((item.accessLevel || 100 ) >= parseInt(helpCookie.getCookie('roleId')) &&
+          {routerList.map((item, key) => { return ((!item.accessLevel || (item.accessLevel) >= parseInt(helpCookie.getCookie('roleId'))) &&
             <div key={key}>
               {item.child ? (
                 <div className="group">
