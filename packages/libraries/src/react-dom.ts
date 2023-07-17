52cb2d57beaf5fa4b95a991d4a103d6b0fa8a2d6
Rizky Novando <rizkynovando@gmail.com>
2023-07-17 14:59:33 +0700

    update library and helper

---
+++ packages/libraries/src/react-dom.ts
@@ -0,0 +1,5 @@
+import * as reactDom from 'react-dom'
+import * as reactDomClient from 'react-dom/client'
+import * as reactDomServer from 'react-dom/server'
+
+export { reactDom, reactDomClient, reactDomServer }
